4dee3ce7115f4c6d243e665d4ef83c31dcbb848d
Admin-PC\Admin <ronanam992@gmail.net>
2017-01-23 16:36:37 +0900

    getter / setter : 필터

--- JavaPro/src/com/ktds/edu/ronanam/student/Student.java
+++ JavaPro/src/com/ktds/edu/ronanam/student/Student.java
@@ -4,23 +4,46 @@ import java.util.Scanner;
 
 public class Student {
 	Scanner sc = new Scanner(System.in);
-	String name;
+	private String name; //getter / setter 이용
 	int age;
 	int score;
 	String food;
 	int weight;
 	byte check;
-	
+
+
+	//shift + alt + s -> generate setter / getter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		//this: 인스턴스필드의
+		if(name.length() == 3){
+			this.name = name;
+		}else{
+			System.out.println("세 글자의 이름 입력하세요.");
+		}
+	}
+
+
+
+	public Student() {
+		//default constructor
+	}
+
+	//instance변수에 대해 setter를 만든다. data 갱신
+
 	public Student(String name, int age, int score, String food, int weight) {
 		this.name = name;
 		this.age = age;
 		this.score = score;
 		this.food = food;
 		this.weight = weight;
-		
+
 		start();
 	}
-	
+
 	private void start() {
 		System.out.println("1. 학생 보기 / 2. 공부하기 / 3. 놀기 / 4. 먹기 / 5. 프로그램 종료");
 		check = sc.nextByte();	
@@ -31,35 +54,35 @@ public class Student {
 		case 4: eating(); break;
 		case 5: System.out.println("!!!프로그램 종료!!!"); System.exit(-1); break;
 		}
-		
+
 	}
 
 	public void viewStudents(){
 		System.out.printf("이름: %s\n나이: %d\n",name, age);
 	}
-	
+
 	public void studying(){
 		System.out.printf("%s 학생의 현재 점수는 %d점입니다.\n",name,score);
 		System.out.println("=================공부 중=================");
 		score += 10;
 		System.out.printf("%s 학생의 점수가 10점 만큼 올라서 현재 %d점입니다.",name, score);
 	}
-	
+
 	public void playing(){
 		System.out.printf("%s 학생의 현재 점수: %d점 / 몸무게: %dkg입니다.\n",name,score,weight);
 		System.out.println("=================노는 중=================");
 		score -= 10;
 		System.out.printf("%s 학생의 점수와 몸무게가 10점씩 감소해 현재 점수: %d / 몸무게: %dkg 입니다."
 				,name, score,weight);
-		
+
 	}
-	
+
 	public void eating(){
 		System.out.printf("%s 학생의 현재 몸무게는 %dkg입니다.");
 		System.out.printf("=================%s를 먹는 중=================",food);
 		weight += 10;
 		System.out.printf("%s 학생의 몸무게는 10증가로 %dkg입니다.",weight);
-		
+
 	}
 
 }
--- JavaPro/src/com/ktds/edu/ronanam/student/StudentMain.java
+++ JavaPro/src/com/ktds/edu/ronanam/student/StudentMain.java
@@ -2,7 +2,13 @@ package com.ktds.edu.ronanam.student;
 
 public class StudentMain {
 	public static void main(String[] args){
-		new Student("남윤민", 26, 55, "망고", 53);
-//		new Student("송규동", 28, 70, "딸기", 77);
+//		new Student("남윤민", 26, 55, "망고", 53);
+		
+		Student student = new Student();
+//		student.name = "손진무"; //private
+		student.setName("손진무");
+		student.setName("규동");
+//		student.viewStudents();
+		
 	}
 }
