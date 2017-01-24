12aa37b9d25a94dc189cb1646cbac8fdd6597b3e
Admin-PC\Admin <ronanam992@gmail.net>
2017-01-24 17:53:06 +0900

    no message

--- JavaPro/src/com/ktds/edu/ronanam/doctor/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ktds.edu.ronanam;
-
-import com.ktds.edu.ronanam.doctor.Doctor;
-
-public class Main {
-	public static void main(String[] args){
-		new Doctor();
-	}
-}
--- JavaPro/src/com/ktds/edu/ronanam/stand/StandMain.java
+++
@@ -1,9 +0,0 @@
-package com.ktds.edu.ronanam.stand;
-
-import java.io.IOException;
-
-public class StandMain {
-	public static void main(String[] args) throws IOException{
-		new Stand(); //시작개체
-	}
-}
